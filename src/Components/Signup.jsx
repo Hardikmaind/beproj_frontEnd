5e9357a9943f521877b9fb2124f927b667562085
Hardikmaind <maindhardik@gmail.com>
2023-12-29 13:46:31 +0530

    color changed

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -60,7 +60,7 @@ function Signup({ showLogin, setShowLogin }) {
                     id="email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)} 
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#2563eb] focus:border-[#2563eb] block w-full p-2.5"
                     placeholder="name@company.com"
                     required=""
                   />
@@ -126,7 +126,7 @@ function Signup({ showLogin, setShowLogin }) {
                 </div>
                 <button
                   type="submit"
-                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+                  className="w-full text-white bg-[#2563eb] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
                 >
                   Create an account
                 </button>
@@ -134,7 +134,7 @@ function Signup({ showLogin, setShowLogin }) {
                   Already have an account?{" "}
                   <button
                     onClick={() => toggleLogin()}
-                    className="font-medium text-primary-600 hover:underline "
+                    className="font-medium text-[#2563eb] hover:underline "
                   >
                     Login here
                   </button>
