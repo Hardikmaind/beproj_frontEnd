1a40bd35bf3e629edfa74d8228d62ab5bdc398f4
Adimanchare <adimanchare143@gmail.com>
2024-01-09 16:24:24 +0530

    layout added

--- src/pages/AfterloginDashboard.jsx
+++ src/pages/AfterloginDashboard.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import Card from "../Components/card";
+import xyz from "./Xyz";
+import { Link } from "react-router-dom";
 
 const AfterloginDashboard = () => {
   return (
     <>
-      <div className=" sticky top-0">
-        <Navbar />
-      </div>
       <div className="flex mt-4 mx-auto text-6xl justify-center">
         {" "}
         Choose Interview Type
       </div>
       <div className="mt-56">
-        <div className="flex flex-col  md:m-0 md:flex-row ">
+        <div className="flex flex-col  md:m-0 md:flex-row " >
           {/* First Part */}
+          <Link to="/xyz">
           <Card />
+          </Link>
+          
           {/* Second Part */}
+          <Link to="/xyz">
           <Card />
+          </Link>
+        
         </div>
       </div>
       {/* <span  className="text-4xl"></span> */}
