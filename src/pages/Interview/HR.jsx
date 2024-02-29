df4099c58219183d7f1f67011fc48b026a3c8ed2
Hardikmaind <maindhardik@gmail.com>
2024-02-29 18:05:33 +0530

    done the hr questions fetching also added the .env file

--- src/pages/Interview/HR.jsx
+++ src/pages/Interview/HR.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 // import AudioRecorder from '../../Components/audiorecordercomponent'
 import QA from '../../Components/QA'
+import useFetch from '../../hooks/useFetch'
+
 
 
 const HR = () => {
+  const { data, loading, error } = useFetch("hr_questions/");
+  if (loading || !data) return []; // Return an empty array while loading or if data is null
+  if (error) {
+    console.error("Error fetching interview questions:", error);
+    return []; // Return an empty array if there's an error
+  }
+  console.log(data);
+
+  console.log("Quesstions", data, loading, error);
   return (
     <div className="bg-gray-100 ">
       <h6 className="text-5xl">HR Interview</h6>
-      <QA/>
+      <QA data={data}/>
     </div>
   )
 }
