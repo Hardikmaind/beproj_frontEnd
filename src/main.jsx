ca246d51c411deeee8b0ba30d346f61472404f8f
Hardikmaind <maindhardik@gmail.com>
2024-03-16 15:05:39 +0530

    removed the strict mode..so that api can be called faster

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import { AuthProvider } from "./context/AuthContext";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
